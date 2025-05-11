1c416ae38aee6712cea498bf1b4ba9505eb577a4
Furkan Baytekin <furkanbaytekin@gmail.com>
2025-05-11 21:20:50 +0300

    fix: exit bug & feat: acceleration by player count

--- entities/ship/scripts/pad.gd
+++ entities/ship/scripts/pad.gd
@@ -2,19 +2,17 @@ extends Area2D
 
 @export var direction: Vector2
 
-@onready var is_active: bool = false
 @onready var parent := $"../.."
+@onready var players_on_it = []
 
 func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("player"):
-		is_active = true
-		$Collision.modulate = Color(1, 0, 1)
+		players_on_it.append(body)
 
 func _on_body_exited(body: Node2D) -> void:
 	if body.is_in_group("player"):
-		is_active = false
-		$Collision.modulate = Color(1, 1, 1)
+		players_on_it.erase(body)
+		
 
 func _physics_process(delta: float) -> void:
-	if is_active:
-		parent.move_and_collide(direction * delta * parent.speed)
+	parent.move_and_collide(direction * delta * parent.speed * len(players_on_it))
