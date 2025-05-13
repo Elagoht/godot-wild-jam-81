df501c136b84e21c5234dd160779d4965de31df9
Furkan Baytekin <furkanbaytekin@gmail.com>
2025-05-13 22:29:55 +0300

    feat: implement health system for ship with debug controls and add status bar

---
+++ entities/ship/healthbar/status_bar.gd
@@ -0,0 +1,4 @@
+extends Node2D
+
+@export var max_hp: float = 100
+@onready var hp: float = max_hp
--- entities/ship/scripts/ship.gd
+++ entities/ship/scripts/ship.gd
@@ -1,3 +1,31 @@
 extends CharacterBody2D
 
 @export var speed = 50
+@export var max_hp: float = 100
+@onready var hp: float = max_hp
+
+@onready var status_bar: Node2D = $StatusBar
+
+signal hp_changed(new_hp: float)
+
+func on_hp_changed(new_hp: float):
+	hp = new_hp
+	hp_changed.emit(hp)
+
+func take_damage(amount: float):
+	hp -= amount
+	if hp < 0:
+		hp = 0
+	hp_changed.emit(hp)
+
+func heal(amount: float):
+	hp += amount
+	if hp > max_hp:
+		hp = max_hp
+	hp_changed.emit(hp)
+	
+func _process(_delta: float) -> void:
+	if Input.is_action_just_pressed("debug_hp_down"):
+		take_damage(10)
+	if Input.is_action_just_pressed("debug_hp_up"):
+		heal(10)
