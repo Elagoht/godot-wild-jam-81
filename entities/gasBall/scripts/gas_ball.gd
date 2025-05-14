830b9894f6b15688a5ec3d06fc17e70cfc0028f8
Furkan Baytekin <furkanbaytekin@gmail.com>
2025-05-14 22:56:31 +0300

    feat: connect GasBall entity to player damage system on body enter

--- entities/gasBall/scripts/gas_ball.gd
+++ entities/gasBall/scripts/gas_ball.gd
@@ -7,6 +7,7 @@ extends StaticBody2D
 
 func _ready() -> void:
 	gass.scale = Vector2(0, 0)
+	gass.body_entered.connect(_on_gass_body_entered)
 
 func _process(delta: float) -> void:
 	var target_scale = Vector2(
@@ -18,3 +19,7 @@ func _process(delta: float) -> void:
 
 func _on_timer_timeout() -> void:
 	is_growing = not is_growing
+
+func _on_gass_body_entered(body):
+	if body.is_in_group("player"):
+		body.take_damage(10)
